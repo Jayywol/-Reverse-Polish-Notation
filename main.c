051b243b0eb413564be4d6efebb6b2278bda5b25
Jayywol <ma_alexandre@outlook.com>
2019-10-22 19:30:28 +0200

    Initial code

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        char instruction[11];
+        scanf("%s", instruction);
+    }
+
+    // Write an action using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
