5c4233591b474f2e88a535e0db6043e7ed642285
Jayywol <ma_alexandre@outlook.com>
2019-10-23 14:46:50 +0200

    Add comment

--- main.c
+++ main.c
@@ -2,17 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+** Structure d'une liste chainee
+*/
+
 typedef struct maillon_t
 {
   int valeur;
   struct maillon_t* suivant;
 }maillon_t;
 
+/*
+** Methode permettant d'exit avec affichage d'erreur
+*/
+
 void my_error()
 {
     fprintf(stderr, "Erreur\n");
     exit(1);
 }
+
+/*
+** Fonction permettant la creation d'un seul maillon
+*/
 
 maillon_t* create_maillon(int valeur, maillon_t* suivant)
 {
@@ -23,6 +35,10 @@ maillon_t* create_maillon(int valeur, maillon_t* suivant)
     maillon->suivant = suivant;
     return maillon;
 }
+
+/*
+** Fonction principale (main)
+*/
 
 int main()
 {
