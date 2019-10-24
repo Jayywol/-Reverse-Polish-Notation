5c5d989f41213b1ffd4eea0d182170d158602609
Jayywol <ma_alexandre@outlook.com>
2019-10-24 13:06:17 +0200

    Add function is_alpha()

--- main.c
+++ main.c
@@ -9,7 +9,17 @@ typedef struct maillon_t
   struct maillon_t* suivant;
 }maillon_t;
 
-//Fonction permettant de verifier si la chaine de caractere est un chiffre
+//Fonction permettant de verifier si la chaine de caractere est une lettre/mot
+int is_alpha(char *str)
+{
+  int i = 0;
+  while (str[i++])
+    if (isalpha(str[i]))
+      return (1);
+  return (0);
+}
+
+//Fonction permettant de verifier si la chaine de caractere est un chiffre/nombre
 int is_num(char *str)
 {
   int i = 0;
